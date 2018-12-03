f7ea2d71971a9f456e5cb61e0ca2970cc2e3b48d
tadeusz.cwichula <tadeusz.cwichula@infosys.com>
2018-12-03 10:44:06 +0100

    project init

---
+++ src/test/java/com/infosys/ceh/CehApplicationTests.java
@@ -0,0 +1,16 @@
+package com.infosys.ceh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CehApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
