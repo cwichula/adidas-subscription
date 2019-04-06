d49cb96cd01b07a0ddc152a9775bcdea066d42e6
Tadeusz Cwichula <cwichula@gmail.com>
2019-04-06 14:39:25 +0200

    security credentials are now stateless

--- src/main/java/com/example/endpoint/SecurityConfig.java
+++ src/main/java/com/example/endpoint/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -15,7 +16,10 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
-                .httpBasic();
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Autowired
